2726322961737492bd1151dad4305857354dd4ae
sofia gil <sofia.gil@uabc.edu.mx>
2019-03-13 12:30:05 -0700

    make it better

--- src/main/java/com/uabc/edu/mx/pruebaspring/Controller/citas.java
+++ src/main/java/com/uabc/edu/mx/pruebaspring/Controller/citas.java
@@ -3,14 +3,10 @@ package com.uabc.edu.mx.pruebaspring.Controller;
 import com.uabc.edu.mx.pruebaspring.Modelo.mModelo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 
-
-@Controller
+@RestController 
 
 
 
